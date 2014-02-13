7e38298849a02e827a344aa40f296bcff4bc1600
Morten Egan <morten@egan.dk>
2014-02-13 10:27:38 +0100

    Install file for github_utl packages

---
+++ github_utl_load.sql
@@ -0,0 +1,34 @@
+-- Specs
+@@json.spec.sql
+@@github.spec.sql
+@@github_repos.spec.sql
+@@github_repos_content.spec.sql
+@@github_issues_milestones.spec.sql
+@@github_repos_merge.spec.sql
+@@github_repos_forks.spec.sql
+@@github_issues_assignees.spec.sql     
+@@github_repos_releases.spec.sql
+@@github_issues_comments.spec.sql      
+@@github_issues_events.spec.sql        
+@@github_tables.spec.sql
+@@github_issues_labels.spec.sql
+@@github_issues.spec.sql
+@@github_repos_collaborators.spec.sql
+@@github_repos_commits.spec.sql   
+-- Bodies
+@@json.body.sql
+@@github.body.sql
+@@github_issues.body.sql  
+@@github_repos_forks.body.sql   
+@@github_repos_merge.body.sql
+@@github_issues_assignees.body.sql
+@@github_repos_releases.body.sql
+@@github_issues_comments.body.sql
+@@github_issues_events.body.sql
+@@github_tables.body.sql
+@@github_issues_labels.body.sql
+@@github_issues_milestones.body.sql    
+@@github_repos.body.sql                
+@@github_repos_collaborators.body.sql  
+@@github_repos_commits.body.sql            
+@@github_repos_content.body.sql        
